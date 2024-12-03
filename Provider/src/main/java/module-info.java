ae37aaf5291bde29e4ac6d6748e18cdb76a0c423
Rishad Mahmudi <rismah1102@gmail.com>
2024-12-03 15:07:02 +0100

    Added GBPConverter

--- Provider/src/main/java/module-info.java
+++ Provider/src/main/java/module-info.java
@@ -1,8 +1,9 @@
+import org.example.Provider.GBPConverter;
 import org.example.Provider.SEKConverter;
 import org.example.Provider.USDConverter;
 import org.example.service.CurrencyConverter;
 
 module org.example.Provider {
     requires Service;
-    provides CurrencyConverter with USDConverter, SEKConverter;
+    provides CurrencyConverter with USDConverter, SEKConverter, GBPConverter;
 }
---
+++ Provider/src/main/java/org/example/Provider/GBPConverter.java
@@ -0,0 +1,22 @@
+package org.example.Provider;
+
+import org.example.service.CurrencyConverter;
+
+public class GBPConverter implements CurrencyConverter {
+    @Override
+    public double convertTo(String toCurrency, double amount) {
+        switch (toCurrency.toUpperCase()) {
+            case "USD":
+                return amount * 1.33; // GBP -> USD
+            case "SEK":
+                return amount * 14.0; // GBP -> SEK
+            default:
+                throw new IllegalArgumentException("Unsupported currency: " + toCurrency);
+        }
+    }
+
+    @Override
+    public String getBaseCurrency() {
+        return "GBP";
+    }
+}
